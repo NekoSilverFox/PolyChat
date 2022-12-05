71da9449340e0ddfcaf4cc29e4203018ee538861
NekoSilverFox <weidufox@gmail.com>
2022-12-06 00:47:58 +0300

    Add UI Login

--- PolyChatApp/main.cpp
+++ PolyChatApp/main.cpp
@@ -1,10 +1,15 @@
 #include "uil_chatboxwidget.h"
 
 #include <QApplication>
+#include <uil_loginwidget.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    LoginWidget login;
+    login.show();
+
     ChatBoxWidget w;
     w.show();
     return a.exec();
