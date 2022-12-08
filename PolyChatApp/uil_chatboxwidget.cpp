786ad646185f71c75109025a3f367f725806b14c
NekoSilverFox <weidufox@gmail.com>
2022-12-08 18:39:10 +0300

    Done: Main Chat funcion

--- PolyChatApp/uil_chatboxwidget.cpp
+++ PolyChatApp/uil_chatboxwidget.cpp
@@ -2,6 +2,7 @@
 #include "ui_chatboxwidget.h"
 #include "dal_polychat.h"
 #include <QDataStream>
+#include <QDateTime>
 
 ChatBoxWidget::ChatBoxWidget(QWidget* parent, QString name, qint16 port)
     : QWidget(parent)
@@ -22,6 +23,8 @@ ChatBoxWidget::ChatBoxWidget(QWidget* parent, QString name, qint16 port)
     this->udpSocketOnPortChatBox = new QUdpSocket(this);
     this->udpSocketOnPortChatBox->bind(this->port,
                                        QUdpSocket::ShareAddress | QUdpSocket::ReuseAddressHint);  // 共享地址 + 断线重连
+    connect(udpSocketOnPortChatBox, &QUdpSocket::readyRead,
+            this, &ChatBoxWidget::receiveUDPMessage);
 
     /* 每隔 BORDCAST_TIME_STEP 毫秒就发送一下当前窗口打开的信息 */
     QTimer* timer = new QTimer(this);
@@ -35,6 +38,11 @@ ChatBoxWidget::ChatBoxWidget(QWidget* parent, QString name, qint16 port)
     /* 点击发送按钮发送消息 */
     connect(ui->btnSend, &QPushButton::clicked,
             [=](){sendUDPSignal(SignalType::Msg);});
+
+
+    /* 点击退出按钮，关闭窗口 */
+    connect(ui->btnExit, &QPushButton::clicked,
+            this, [=](){this->close();});
 }
 
 ChatBoxWidget::~ChatBoxWidget()
@@ -79,14 +87,14 @@ void ChatBoxWidget::sendUDPSignal(const SignalType type)
     switch (type) {
 
     case SignalType::ChatExist:
-        dataStream << "SignalType::ChatExist";
+        dataStream << QString("SignalType::ChatExist");
         udpSocketOnPortChatList->writeDatagram(resByteArray,
                                                QHostAddress(QHostAddress::Broadcast),
                                                DAL::getPortChatList());
         break;  // END SignalType::ChatExist
 
     case SignalType::ChatDestory:
-        dataStream << "SignalType::ChatDestory";
+        dataStream <<  QString("SignalType::ChatDestory");
         udpSocketOnPortChatList->writeDatagram(resByteArray,
                                                QHostAddress(QHostAddress::Broadcast),
                                                DAL::getPortChatList());
@@ -101,30 +109,123 @@ void ChatBoxWidget::sendUDPSignal(const SignalType type)
                                               port);
         break;  // END SignalType::Msg
 
-    case UserJoin:
+    case SignalType::UserJoin:
+        dataStream <<  QString("SignalType::UserJoin");
         udpSocketOnPortChatBox->writeDatagram(resByteArray,
                                               QHostAddress(QHostAddress::Broadcast),
                                               port);
         break;
 
     case UserLeft:
-//        udpSocketOnPortChatBox->writeDatagram(resByteArray,
-//                                              QHostAddress(QHostAddress::Broadcast),
-//                                              port);  还要添加 closeEvent！！！！ TODO
+        dataStream <<  QString("SignalType::UserLeft");
+        udpSocketOnPortChatBox->writeDatagram(resByteArray,
+                                              QHostAddress(QHostAddress::Broadcast),
+                                              port);
         break;
 
     default:
         break;
     }
-
-    qDebug() << "ChatBoxWidget send signal on port:" << udpSocketOnPortChatList
-             << "SignalType::" << type
-             << chatName_2 << chatPort_3
-             << localUserName_4
-             << localUserGroupNumber_5
-             << localIpAddress_6;
 }
 
+/** 接收 UDP 消息
+ * @brief receiveMessage
+ */
+void ChatBoxWidget::receiveUDPMessage()
+{
+    /* 拿到数据报文 */
+    qint64 msgSize = udpSocketOnPortChatBox->pendingDatagramSize();
+    QByteArray resByteArray = QByteArray(msgSize, 0);
+    udpSocketOnPortChatBox->readDatagram(resByteArray.data(), msgSize);
+
+    /** 解析数据 - 消息分为 5 类 (SignalType)，所以要对数据做分段处理
+    *   第1段：数据类型
+    *   第2段：发送本信号的群组名称
+    *   第3段：发送本信号的群组端口号
+    *   第4段：当前（本地）用户名 localUserName
+    *   第5段：当前（本地）用户组号 localUserGroupNumber
+    *   第6段：当前（本地）用户ip localIpAddress
+    *   第7段：具体内容 QString
+    */
+    SignalType      signalType_1            ;
+    QString         chatName_2              ;
+    qint16          chatPort_3              ;
+    QString         localUserName_4         ;
+    QString         localUserGroupNumber_5  ;
+    QHostAddress    localIpAddress_6        ;
+    QString         msg_7;
+
+    QDataStream dataStream(&resByteArray, QIODevice::ReadOnly);
+    QString time = QDateTime::currentDateTime().toString("dd-MM-yyyy hh:mm:ss");
+
+    dataStream >> signalType_1;             // 第1段：数据类型
+    dataStream >> chatName_2;               // 第2段：发送信号（本）信号的群组名称
+    dataStream >> chatPort_3;               // 第3段：发送信号（本）群组端口号
+    dataStream >> localUserName_4;          // 第4段：发送信号用户名
+    dataStream >> localUserGroupNumber_5;   // 第5段：发送信号用户组号
+    dataStream >> localIpAddress_6;         // 第6段：发送信号用户ip
+    dataStream >> msg_7;                    // 第7段：具体内容 QString
+
+    switch (signalType_1) {
+    case SignalType::Msg:
+        // 追加聊天记录
+        ui->msgTextBrowser->setTextColor(Qt::blue);
+        ui->msgTextBrowser->append("[" + localUserName_4 + "] " + time);
+        ui->msgTextBrowser->append(msg_7);
+        break;
+
+    case SignalType::UserJoin:
+        userJoin(localUserName_4);
+        break;
+
+    case SignalType::UserLeft:
+        userLeft(localUserName_4, time);
+        break;
+
+    default:
+        break;
+    }
+}
+
+/* 处理用户加入 */
+void ChatBoxWidget::userJoin(QString name)
+{
+    if (ui->tbUser->findItems(name, Qt::MatchExactly).isEmpty())
+    {
+        /* 更新用户列表 */
+        QTableWidgetItem* user = new QTableWidgetItem(name);
+        ui->tbUser->insertRow(0);
+        ui->tbUser->setItem(0, 0, user);
+
+        /* 追加聊天记录 */
+        ui->msgTextBrowser->setTextColor(Qt::gray);
+        ui->msgTextBrowser->append(QString("%1 online！").arg(name));
+
+        /* 在线用户更新 */
+        ui->lbNumberOnlineUse->setText(QString("Number of online user: %1").arg(ui->tbUser->rowCount()));
+
+        /* 把自身信息广播 */
+        sendUDPSignal(SignalType::UserJoin);
+    }
+}
+
+
+/* 处理用户离开 */
+void ChatBoxWidget::userLeft(QString name, QString time)
+{
+    if (!ui->tbUser->findItems(name, Qt::MatchExactly).isEmpty())
+    {
+        /* 更新用户列表 */
+        ui->tbUser->removeRow(ui->tbUser->findItems(name, Qt::MatchExactly).first()->row());
+
+        /* 追加聊天记录 */
+        ui->msgTextBrowser->setTextColor(Qt::gray);
+        ui->msgTextBrowser->append(QString("%1 left %2 on ").arg(name).arg(time));
+
+        /* 在线用户更新 */
+        ui->lbNumberOnlineUse->setText(QString("Number of online user：%1").arg(ui->tbUser->rowCount()));
+    }
+}
 
 QString ChatBoxWidget::getAndCleanMsg()
 {
@@ -142,6 +243,8 @@ QString ChatBoxWidget::getAndCleanMsg()
 void ChatBoxWidget::closeEvent(QCloseEvent* event)
 {
     emit this->signalClose();
+
+
 
     sendUDPSignal(SignalType::UserLeft);  // TODO 如果是最后一个用户则应该发出销毁指令
 
